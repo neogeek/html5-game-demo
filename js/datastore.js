2c9c4f0b94d8e0e9d4f9acdabddea7801fa0ecd2
Scott Doxey <neogeek0101@gmail.com>
2016-10-06 18:02:24 -0400

    Removed console.log

--- js/datastore.js
+++ js/datastore.js
@@ -2,8 +2,6 @@
 
 var Firebase = require('firebase');
 
-console.log(Firebase);
-
 var myFirebaseRef = new Firebase('https://html5-game-demo.firebaseio.com/players/');
 
 module.exports = myFirebaseRef;
