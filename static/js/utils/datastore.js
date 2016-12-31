0a78abd63293575b054b1546700a05ae0a1d8e50
Scott Doxey <neogeek0101@gmail.com>
2016-12-31 03:03:23 -0500

    Removed unused keys.

--- static/js/utils/datastore.js
+++ static/js/utils/datastore.js
@@ -1,11 +1,7 @@
 const firebase = require('firebase');
 
 firebase.initializeApp({
-    'apiKey': 'AIzaSyBTkqUxvhXJ6UcfmmHAlTS9_Qxp8Y03nJo',
-    'authDomain': 'html5-game-demo.firebaseapp.com',
-    'databaseURL': 'https://html5-game-demo.firebaseio.com',
-    'messagingSenderId': '684890656119',
-    'storageBucket': 'html5-game-demo.appspot.com'
+    'databaseURL': 'https://html5-game-demo.firebaseio.com'
 });
 
 module.exports = firebase.database().ref('players');
